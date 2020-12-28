7d7f54254357a9839d585f10eea5b6c6450ef617
Ramsey lewis <ramseylewis99@gmail.com>
2020-12-27 21:06:53 -0500

    setup

---
+++ websiterjl/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import loadingPage from './loadingPage/loadingPage'
+
+function App() {
+  return (
+    <div className="App">
+      <loadingPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ websiterjl/src/loadingPage/loadingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './loadingPage.css'
+
+function loadingPage() {
+    return (
+        <div className='Test'>
+            <div> Test Test Test</div>
+        </div>
+    )
+}
+
+export default loadingPage
